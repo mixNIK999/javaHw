5cfec768c5dc3257ae0a8be82b16a88560cd2b08
Mikhail Nikolyukin <michael.nik999@gmail.com>
2019-01-29 13:29:13 +0300

    add Node implementation

---
+++ Homework_1/src/main/java/me/nikolyukin/homework1/HashMap.java
@@ -0,0 +1,39 @@
+package me.nikolyukin.homework1;
+
+public class HashMap {
+    private class List {
+
+        Node head;
+        int size;
+
+        private class Node {
+            private String value;
+            private String key;
+            private Node next;
+
+            public Node() {}
+
+            public Node(String value, String key, Node next) {
+                this.value = value;
+                this.key = key;
+                this.next = next;
+            }
+
+            public Node getNext() {
+                return next;
+            }
+
+            public String getKey() {
+                return key;
+            }
+
+            public String getValue() {
+                return value;
+            }
+
+            public void setValue(String value) {
+                this.value = value;
+            }
+        }
+    }
+}
