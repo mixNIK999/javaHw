f25e36cc159039c4d0019bb10732692148d9a570
Mikhail Nikolyukin <michael.nik999@gmail.com>
2019-02-05 13:04:57 +0300

    add nested class Node with constructor

--- Homework_2/src/main/java/me/nikolyukin/Trie.java
+++ Homework_2/src/main/java/me/nikolyukin/Trie.java
@@ -3,6 +3,7 @@ package me.nikolyukin;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.LinkedHashMap;
 
 public class Trie implements MySerializable {
 
@@ -23,7 +24,7 @@ public class Trie implements MySerializable {
     }
 
     int howManyStartsWithPrefix(String prefix) {
-        
+
     }
 
     @Override
@@ -34,6 +35,19 @@ public class Trie implements MySerializable {
     @Override
     public void deserialize(InputStream in) throws IOException {
 
+    }
+
+    private static class Node {
+        private LinkedHashMap<Character, Node> children;
+        private Node parent;
+        private Character parantCharacter;
+
+        private Node() {}
+        private Node(Node parent, Character parentCharacter) {
+            this.parent = parent;
+            this.parantCharacter = parentCharacter;
+        }
+        
     }
 }
 
