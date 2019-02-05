f121088beac3afee8197d7654c5c2bbac1035ef1
Mikhail Nikolyukin <michael.nik999@gmail.com>
2019-02-05 12:43:53 +0300

    Create TrieTest.java

---
+++ Homework_2/src/test/java/me/nikolyukin/TrieTest.java
@@ -0,0 +1,34 @@
+package me.nikolyukin;
+
+import org.junit.jupiter.api.Test;
+
+class TrieTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void howManyStartsWithPrefix() {
+    }
+
+    @Test
+    void serialize() {
+    }
+
+    @Test
+    void deserialize() {
+    }
+}
