d2b684a07ed723e22839372e4ddeaaee9b7f9f9f
Mihail Nikolyukin <michael.nik999@gmail.com>
2019-02-06 16:19:09 +0300

    add get, set, size

--- CR1/src/main/java/me/nikolyukin/SmartList.java
+++ CR1/src/main/java/me/nikolyukin/SmartList.java
@@ -1,4 +1,46 @@
 package me.nikolyukin;
 
-public class SmartList {
+import java.util.AbstractList;
+import java.util.ArrayList;
+
+public class SmartList<E> extends AbstractList<E> {
+    private int listSize = 0;
+    private Object container;
+
+//    private void resize(int newSize) {
+//
+//    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (size == 1) {
+            return (E)container;
+        } else if (size <= 5){
+            return ((E[])container)[index];
+        } else {
+            return ((ArrayList<E>)container).get(index);
+        }
+    }
+
+    public int size() {
+        return listSize;
+    }
+
+    public E set​(int index, E element) {
+        E prev;
+        if (size == 1) {
+            prev = (E)container;
+            container = element;
+        } else if (size <= 5){
+            prev = ((E[])container)[index];
+            ((E[])container)[index] = element;
+        } else {
+            prev = ((ArrayList<E>)container).set(index, element);
+        }
+        return prev;
+    }
+
+
 }
