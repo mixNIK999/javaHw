21f044c03a90d7a6b9177597cbb7be82b6352511
Mikhail Nikolyukin <michael.nik999@gmail.com>
2019-02-13 02:48:43 +0300

    Create MyTreeSetImplementation.java

---
+++ Homework_3/src/main/java/me/nikolyukin/homework3/MyTreeSetImplementation.java
@@ -0,0 +1,114 @@
+package me.nikolyukin.homework3;
+
+import java.util.AbstractSet;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class MyTreeSetImplementation<E> extends AbstractSet<E> implements MyTreeSet<E> {
+    Node<E> root;
+    
+    /**
+     * Returns an iterator over the elements contained in this collection.
+     *
+     * @return an iterator over the elements contained in this collection
+     */
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws UnsupportedOperationException {@inheritDoc}
+     * @throws ClassCastException {@inheritDoc}
+     * @throws NullPointerException {@inheritDoc}
+     * @throws IllegalArgumentException {@inheritDoc}
+     * @throws IllegalStateException {@inheritDoc}
+     * @implSpec This implementation always throws an {@code UnsupportedOperationException}.
+     */
+    @Override
+    public boolean add(E e) {
+        return super.add(e);
+    }
+
+    /**
+     * {@link TreeSet#descendingIterator()}
+     **/
+    @Override
+    public Iterator<E> descendingIterator() {
+        return null;
+    }
+
+    /**
+     * {@link TreeSet#descendingSet()}
+     **/
+    @Override
+    public MyTreeSet<E> descendingSet() {
+        return null;
+    }
+
+    /**
+     * {@link TreeSet#first()}
+     **/
+    @Override
+    public E first() {
+        return null;
+    }
+
+    /**
+     * {@link TreeSet#last()}
+     **/
+    @Override
+    public E last() {
+        return null;
+    }
+
+    /**
+     * {@link TreeSet#lower(Object)}
+     **/
+    @Override
+    public E lower(E e) {
+        return null;
+    }
+
+    /**
+     * {@link TreeSet#lower(Object)}
+     **/
+    @Override
+    public E floor(E e) {
+        return null;
+    }
+
+    /**
+     * {@link TreeSet#ceiling(Object)}
+     **/
+    @Override
+    public E ceiling(E e) {
+        return null;
+    }
+
+    /**
+     * {@link TreeSet#higher(Object)}
+     **/
+    @Override
+    public E higher(E e) {
+        return null;
+    }
+
+    private static class Node<E> {
+        private Node<E> LeftChild;
+        private Node<E> RightChild;
+        private E value;
+
+        private Node(E value) {
+            this.value = value;
+        }
+    }
+
+}
