d02201bab1b457b9e7a53ce53588c6bcd39c3442
Mikhail Nikolyukin <michael.nik999@gmail.com>
2019-02-13 06:53:27 +0300

    add tests for iterator

--- Homework_3/src/test/java/me/nikolyukin/homework3/MyTreeSetImplementationTest.java
+++ Homework_3/src/test/java/me/nikolyukin/homework3/MyTreeSetImplementationTest.java
@@ -2,12 +2,28 @@ package me.nikolyukin.homework3;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Comparator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MyTreeSetImplementationTest {
-
+    private MyTreeSet<Integer> emptyTreeSet;
+    private MyTreeSet<Integer> TreeSet532;
+    @BeforeEach
+    void init() {
+        emptyTreeSet = new MyTreeSetImplementation<>();
+        TreeSet532 = new MyTreeSetImplementation<>(Comparator.reverseOrder());
+        TreeSet532.add(2);
+//        TreeSet532.add(5);
+//        TreeSet532.add(3);
+    }
     @Test
-    void iterator() {
+    void iteratorHasNextTrue() {
+//        assertTrue(TreeSet532.iterator().hasNext());
+    }
+    @Test
+    void iteratorHasNextFalse() {
+        assertFalse(emptyTreeSet.iterator().hasNext());
     }
 
     @Test
