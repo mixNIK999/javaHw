98b6daee31ec6c19f4c479bd72a2e577a818264b
Mikhail Nikolyukin <michael.nik999@gmail.com>
2019-02-13 09:12:51 +0300

    add final tests

--- Homework_3/src/test/java/me/nikolyukin/homework3/MyTreeSetImplementationTest.java
+++ Homework_3/src/test/java/me/nikolyukin/homework3/MyTreeSetImplementationTest.java
@@ -8,14 +8,14 @@ import org.junit.jupiter.api.Test;
 
 class MyTreeSetImplementationTest {
     private MyTreeSet<Integer> emptyTreeSet;
-    private MyTreeSet<Integer> TreeSet532;
+    private MyTreeSet<Integer> treeSet532;
     @BeforeEach
     void init() {
         emptyTreeSet = new MyTreeSetImplementation<>();
-        TreeSet532 = new MyTreeSetImplementation<>(Comparator.reverseOrder());
-        TreeSet532.add(2);
-        TreeSet532.add(5);
-        TreeSet532.add(3);
+        treeSet532 = new MyTreeSetImplementation<>(Comparator.reverseOrder());
+        treeSet532.add(2);
+        treeSet532.add(5);
+        treeSet532.add(3);
     }
 
     @Test
@@ -25,12 +25,12 @@ class MyTreeSetImplementationTest {
 
     @Test
     void iteratorHasNextTrue() {
-        assertTrue(TreeSet532.iterator().hasNext());
+        assertTrue(treeSet532.iterator().hasNext());
     }
 
     @Test
     void iteratorNext532() {
-        var iterator = TreeSet532.iterator();
+        var iterator = treeSet532.iterator();
         assertEquals(Integer.valueOf(5), iterator.next());
         assertEquals(Integer.valueOf(3), iterator.next());
         assertEquals(Integer.valueOf(2), iterator.next());
@@ -43,19 +43,19 @@ class MyTreeSetImplementationTest {
 
     @Test
     void sizeNotEmpty() {
-        assertEquals(3, TreeSet532.size());
+        assertEquals(3, treeSet532.size());
     }
 
     @Test
     void addTrue() {
-        assertTrue(TreeSet532.add(8));
-        assertEquals(4,TreeSet532.size());
+        assertTrue(treeSet532.add(8));
+        assertEquals(4, treeSet532.size());
     }
 
     @Test
     void addFalse() {
-        assertFalse(TreeSet532.add(5));
-        assertEquals(3,TreeSet532.size());
+        assertFalse(treeSet532.add(5));
+        assertEquals(3, treeSet532.size());
     }
 
     @Test
@@ -65,12 +65,12 @@ class MyTreeSetImplementationTest {
 
     @Test
     void descendingIteratorHasNextTrue() {
-        assertTrue(TreeSet532.descendingIterator().hasNext());
+        assertTrue(treeSet532.descendingIterator().hasNext());
     }
 
     @Test
     void descendingIteratorNext532() {
-        var descendingIterator = TreeSet532.descendingIterator();
+        var descendingIterator = treeSet532.descendingIterator();
         assertEquals(Integer.valueOf(2), descendingIterator.next());
         assertEquals(Integer.valueOf(3), descendingIterator.next());
         assertEquals(Integer.valueOf(5), descendingIterator.next());
@@ -78,33 +78,69 @@ class MyTreeSetImplementationTest {
     
     @Test
     void descendingSet() {
-        var iterator = TreeSet532.descendingSet().iterator();
+        var iterator = treeSet532.descendingSet().iterator();
         assertEquals(Integer.valueOf(2), iterator.next());
         assertEquals(Integer.valueOf(3), iterator.next());
         assertEquals(Integer.valueOf(5), iterator.next());
     }
 
     @Test
-    void first() {
+    void firstNull() {
+        assertNull(emptyTreeSet.first());
     }
 
     @Test
-    void last() {
+    void firstNotNull() {
+        assertEquals(Integer.valueOf(5), treeSet532.first());
     }
 
     @Test
-    void lower() {
+    void lastNull() {
+        assertNull(emptyTreeSet.last());
     }
 
     @Test
-    void floor() {
+    void lastNotNull() {
+        assertEquals(Integer.valueOf(2), treeSet532.last());
     }
 
     @Test
-    void ceiling() {
+    void lowerNull() {
+        assertNull(emptyTreeSet.lower(1));
     }
 
     @Test
-    void higher() {
+    void lowerNotNull() {
+        assertEquals(Integer.valueOf(5), treeSet532.lower(3));
+    }
+
+    @Test
+    void floorNull() {
+        assertNull(emptyTreeSet.floor(1));
+    }
+
+    @Test
+    void floorNotNull() {
+        assertEquals(Integer.valueOf(3), treeSet532.floor(3));
+    }
+
+    @Test
+    void ceilingNull() {
+        assertNull(emptyTreeSet.ceiling(1));
+    }
+
+    @Test
+    void ceilingNotNull() {
+        assertEquals(Integer.valueOf(3), treeSet532.ceiling(3));
+    }
+
+    @Test
+    void higherNull() {
+        assertNull(emptyTreeSet.higher(1));
+    }
+
+    @Test
+    void higherNotNull() {
+        assertEquals(Integer.valueOf(2), treeSet532.higher(3));
     }
 }
