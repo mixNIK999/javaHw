e3d53b298a3032eccf6cabd75ddda5c8b2b33c97
Mikhail Nikolyukin <michael.nik999@gmail.com>
2019-02-13 05:07:21 +0300

    add lowerBoundNode

--- Homework_3/src/main/java/me/nikolyukin/homework3/MyTreeSetImplementation.java
+++ Homework_3/src/main/java/me/nikolyukin/homework3/MyTreeSetImplementation.java
@@ -3,11 +3,10 @@ package me.nikolyukin.homework3;
 import java.util.AbstractSet;
 import java.util.Comparator;
 import java.util.Iterator;
-import java.util.ListIterator;
 import java.util.NoSuchElementException;
 import java.util.TreeSet;
 
-public class MyTreeSetImplementation<E extends > extends AbstractSet<E> implements MyTreeSet<E> {
+public class MyTreeSetImplementation<E> extends AbstractSet<E> implements MyTreeSet<E> {
     int size = 0;
     Node<E> root = new Node<>();
     private Comparator<? super E> comparator;
@@ -114,8 +113,26 @@ public class MyTreeSetImplementation<E extends > extends AbstractSet<E> implemen
         return null;
     }
 
-    private class TreeIterator<E> implements ListIterator<E> {
-        Node<E> nextElement;
+    private Node<E> lowerBoundNode(E element) {
+        var currentNode = root;
+        var nextNode = root.leftChild;
+        while (nextNode != null) {
+            currentNode = nextNode;
+            int compareRes = comparator.compare(element, currentNode.value);
+            if (compareRes > 0) {
+                nextNode = currentNode.rightChild;
+            } else if (compareRes == 0) {
+                return currentNode;
+            } else {
+                nextNode = currentNode.leftChild;
+            }
+        }
+        return (comparator.compare(element, currentNode.value) >= 0) ? currentNode
+            : currentNode.goPrev();
+    }
+    
+    private class TreeIterator<T> implements Iterator<T> {
+        Node<T> nextElement;
 
         /**
          * Returns {@code true} if this list iterator has more elements when traversing the list in
@@ -140,7 +157,7 @@ public class MyTreeSetImplementation<E extends > extends AbstractSet<E> implemen
          * @throws NoSuchElementException if the iteration has no next element
          */
         @Override
-        public E next() {
+        public T next() {
             if (!hasNext()) {
                 throw new NoSuchElementException();
             }
@@ -157,7 +174,6 @@ public class MyTreeSetImplementation<E extends > extends AbstractSet<E> implemen
          * @return {@code true} if the list iterator has more elements when traversing the list in
          * the reverse direction
          */
-        @Override
         public boolean hasPrevious() {
             return nextElement.goPrev() != root;
         }
@@ -171,96 +187,12 @@ public class MyTreeSetImplementation<E extends > extends AbstractSet<E> implemen
          * @return the previous element in the list
          * @throws NoSuchElementException if the iteration has no previous element
          */
-        @Override
-        public E previous() {
-            return null;
-        }
-
-        /**
-         * Returns the index of the element that would be returned by a subsequent call to {@link
-         * #next}. (Returns list size if the list iterator is at the end of the list.)
-         *
-         * @return the index of the element that would be returned by a subsequent call to {@code
-         * next}, or list size if the list iterator is at the end of the list
-         */
-        @Override
-        public int nextIndex() {
-            return 0;
-        }
-
-        /**
-         * Returns the index of the element that would be returned by a subsequent call to {@link
-         * #previous}. (Returns -1 if the list iterator is at the beginning of the list.)
-         *
-         * @return the index of the element that would be returned by a subsequent call to {@code
-         * previous}, or -1 if the list iterator is at the beginning of the list
-         */
-        @Override
-        public int previousIndex() {
-            return 0;
-        }
-
-//        /**
-//         * Removes from the list the last element that was returned by {@link #next} or {@link
-//         * #previous} (optional operation).  This call can only be made once per call to {@code
-//         * next} or {@code previous}. It can be made only if {@link #add} has not been called after
-//         * the last call to {@code next} or {@code previous}.
-//         *
-//         * @throws UnsupportedOperationException if the {@code remove} operation is not supported by
-//         * this list iterator
-//         * @throws IllegalStateException if neither {@code next} nor {@code previous} have been
-//         * called, or {@code remove} or {@code add} have been called after the last call to {@code
-//         * next} or {@code previous}
-//         */
-        @Override
-        public void remove() {
-
-        }
-
-        /**
-         * Replaces the last element returned by {@link #next} or {@link #previous} with the
-         * specified element (optional operation). This call can be made only if neither {@link
-         * #remove} nor {@link #add} have been called after the last call to {@code next} or {@code
-         * previous}.
-         *
-         * @param e the element with which to replace the last element returned by {@code next} or
-         * {@code previous}
-         * @throws UnsupportedOperationException if the {@code set} operation is not supported by
-         * this list iterator
-         * @throws ClassCastException if the class of the specified element prevents it from being
-         * added to this list
-         * @throws IllegalArgumentException if some aspect of the specified element prevents it from
-         * being added to this list
-         * @throws IllegalStateException if neither {@code next} nor {@code previous} have been
-         * called, or {@code remove} or {@code add} have been called after the last call to {@code
-         * next} or {@code previous}
-         */
-        @Override
-        public void set(E e) {
-
-        }
-
-        /**
-         * Inserts the specified element into the list (optional operation). The element is inserted
-         * immediately before the element that would be returned by {@link #next}, if any, and after
-         * the element that would be returned by {@link #previous}, if any.  (If the list contains
-         * no elements, the new element becomes the sole element on the list.)  The new element is
-         * inserted before the implicit cursor: a subsequent call to {@code next} would be
-         * unaffected, and a subsequent call to {@code previous} would return the new element. (This
-         * call increases by one the value that would be returned by a call to {@code nextIndex} or
-         * {@code previousIndex}.)
-         *
-         * @param e the element to insert
-         * @throws UnsupportedOperationException if the {@code add} method is not supported by this
-         * list iterator
-         * @throws ClassCastException if the class of the specified element prevents it from being
-         * added to this list
-         * @throws IllegalArgumentException if some aspect of this element prevents it from being
-         * added to this list
-         */
-        @Override
-        public void add(E e) {
-
+        public T previous() {
+            if (!hasPrevious()) {
+                throw new NoSuchElementException();
+            }
+            nextElement = nextElement.goPrev();
+            return nextElement.value;
         }
     }
 
