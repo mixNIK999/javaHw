993f996092910f637d1ab6dd9d2b81710ea02f9a
Michael <michael.nik999@gmail.com>
2019-02-27 01:45:50 +0300

    remake addOne test  to addTwo

--- Homework_5/src/test/java/me/nikolyukin/homework5/PhoneBookTest.java
+++ Homework_5/src/test/java/me/nikolyukin/homework5/PhoneBookTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.List;
+import kotlin.Pair;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,9 +13,7 @@ class PhoneBookTest {
     PhoneBook emptyPhoneBook;
 
     @BeforeEach
-    void init()
-        throws SQLException, ClassNotFoundException, IllegalAccessException, InstantiationException {
-//        Class.forName("com.mysql.jdbc.Driver").newInstance();
+    void init() throws SQLException {
         emptyPhoneBook = new PhoneBook(DriverManager.getConnection("jdbc:sqlite::memory:"));
     }
 
@@ -30,11 +28,13 @@ class PhoneBookTest {
     }
 
     @Test
-    void addOne() throws SQLException {
+    void addTwo() throws SQLException {
         emptyPhoneBook.add("foo", "8 800 555 35 35");
         var res = emptyPhoneBook.getAll();
         assertEquals(1, res.size());
         assertEquals("foo", res.get(0).getFirst());
         assertEquals("8 800 555 35 35", res.get(0).getSecond());
+        emptyPhoneBook.add("bar", "123456789");
+        assertTrue(emptyPhoneBook.getAll().contains(new Pair<>("bar", "123456789")));
     }
 }
