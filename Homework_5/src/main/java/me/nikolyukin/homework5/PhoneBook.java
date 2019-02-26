45510d32515da7b0216aa194fab6308129d83207
Michael <michael.nik999@gmail.com>
2019-02-26 23:12:21 +0300

    add PhoneBook constructor

--- Homework_5/src/main/java/me/nikolyukin/homework5/PhoneBook.java
+++ Homework_5/src/main/java/me/nikolyukin/homework5/PhoneBook.java
@@ -3,14 +3,31 @@ package me.nikolyukin.homework5;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import kotlin.Pair;
 import org.jetbrains.annotations.NotNull;
 
 public class PhoneBook {
-    private String url = "jdbc:sqlite:phoneBook.db";
+    private final String url;
 
-    public void add(@NotNull String name, @NotNull String number) {}
+    public PhoneBook(@NotNull String path) throws SQLException {
+        url = "jdbc:sqlite:" + path;
+        String sql = "CREATE TABLE IF NOT EXISTS phoneBook (\n"
+            + "	id integer PRIMARY KEY,\n"
+            + "	name text NOT NULL,\n"
+            + "	number text NOT NULL\n"
+            + ");";
+        try (Connection conn = DriverManager.getConnection(url)) {
+            try (Statement stmt = conn.createStatement()) {
+                stmt.executeUpdate(sql);
+            }
+        }
+    }
+
+    public void add(@NotNull String name, @NotNull String number) {
+
+    }
 
     public ArrayList<String> findNames(@NotNull String number) {
         return null;
