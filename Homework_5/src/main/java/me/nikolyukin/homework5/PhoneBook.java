0e8ff4a0088f74a0dabc9bac4d164da8e2edbb55
Michael <michael.nik999@gmail.com>
2019-02-26 23:55:40 +0300

    add getAll and add methods

--- Homework_5/src/main/java/me/nikolyukin/homework5/PhoneBook.java
+++ Homework_5/src/main/java/me/nikolyukin/homework5/PhoneBook.java
@@ -2,9 +2,13 @@ package me.nikolyukin.homework5;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import kotlin.Pair;
 import org.jetbrains.annotations.NotNull;
 
@@ -13,7 +17,7 @@ public class PhoneBook {
 
     public PhoneBook(@NotNull String path) throws SQLException {
         url = "jdbc:sqlite:" + path;
-        String sql = "CREATE TABLE IF NOT EXISTS phoneBook (\n"
+        final String sql = "CREATE TABLE IF NOT EXISTS phoneBook (\n"
             + "	id integer PRIMARY KEY,\n"
             + "	name text NOT NULL,\n"
             + "	number text NOT NULL\n"
@@ -25,8 +29,14 @@ public class PhoneBook {
         }
     }
 
-    public void add(@NotNull String name, @NotNull String number) {
-
+    public void add(@NotNull String name, @NotNull String number) throws SQLException {
+        final String sql = "INSERT INTO phoneBook(name,capacity) VALUES(?,?)";
+        try (Connection conn = DriverManager.getConnection(url)) {
+            try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                pstmt.setString(1, name);
+                pstmt.setString(2, number);
+            }
+        }
     }
 
     public ArrayList<String> findNames(@NotNull String number) {
@@ -43,7 +53,19 @@ public class PhoneBook {
 
     public void changeName(@NotNull String name, @NotNull String number) {}
 
-    public ArrayList<Pair<String, String>>  getAll() {
-        return null;
+    public List<Pair<String, String>> getAll() throws SQLException {
+        final String sql = "SELECT name, number FROM phoneBook";
+        var result = new ArrayList<Pair<String, String>>();
+        try (Connection conn = DriverManager.getConnection(url)) {
+            try (Statement stmt = conn.createStatement()) {
+                try (ResultSet rs = stmt.executeQuery(sql)) {
+                    while (rs.next()) {
+                        result.add(new Pair<>(rs.getString("name"),
+                                              rs.getString("number")));
+                    }
+                }
+            }
+        }
+        return result;
     }
 }
