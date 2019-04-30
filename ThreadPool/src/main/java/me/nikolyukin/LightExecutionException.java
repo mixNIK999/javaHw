dc2a4bf709bee706f14fe7a4b0523f74ab7af0c7
Michael <michael.nik999@gmail.com>
2019-05-01 02:46:07 +0300

    delete unnecessary constructors from LightExecutionException

--- ThreadPool/src/main/java/me/nikolyukin/LightExecutionException.java
+++ ThreadPool/src/main/java/me/nikolyukin/LightExecutionException.java
@@ -2,18 +2,7 @@ package me.nikolyukin;
 
 public class LightExecutionException extends Exception {
 
-    public LightExecutionException() {
-    }
-
-    public LightExecutionException(String message) {
-        super(message);
-    }
-
-    public LightExecutionException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public LightExecutionException(Throwable cause) {
+    public LightExecutionException(final Throwable cause) {
         super(cause);
     }
 }
