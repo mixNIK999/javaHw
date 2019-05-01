47271f20f0c6bda88950ff8f079aba407ca30009
Michael <michael.nik999@gmail.com>
2019-05-01 05:18:45 +0300

    add tests: shutdownSimple and countThreads

--- ThreadPool/src/test/java/me/nikolyukin/ThreadPoolImplTest.java
+++ ThreadPool/src/test/java/me/nikolyukin/ThreadPoolImplTest.java
@@ -1,20 +1,29 @@
 package me.nikolyukin;
 
+import static java.lang.Thread.interrupted;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ThreadPoolImplTest {
+
     private ThreadPool singleThreadPool;
+    private final int multiThreadPoolSize = 10;
     private ThreadPool multiThreadPool;
 
     @BeforeEach
     void init() {
         singleThreadPool = new ThreadPoolImpl(1);
-        multiThreadPool = new ThreadPoolImpl(10);
+        multiThreadPool = new ThreadPoolImpl(multiThreadPoolSize);
     }
 
     @AfterEach
@@ -26,7 +35,7 @@ class ThreadPoolImplTest {
     @Test
     void submitSimpleSingle() throws LightExecutionException {
         int n = 20;
-        var singleThreadPoolRes  = new ArrayList<LightFuture<String>>(n);
+        var singleThreadPoolRes = new ArrayList<LightFuture<String>>(n);
         for (int i = 0; i < n; i++) {
             singleThreadPoolRes.add(singleThreadPool.submit(() -> "test"));
         }
@@ -38,9 +47,9 @@ class ThreadPoolImplTest {
     @Test
     void submitSimpleMulti() throws LightExecutionException {
         int n = 20;
-        var multiThreadPoolRes  = new ArrayList<LightFuture<String>>(n);
+        var multiThreadPoolRes = new ArrayList<LightFuture<String>>(n);
         for (int i = 0; i < n; i++) {
-            multiThreadPoolRes.add(singleThreadPool.submit(() -> "test"));
+            multiThreadPoolRes.add(multiThreadPool.submit(() -> "test"));
         }
         for (int i = n - 1; i >= 0; i--) {
             assertEquals("test", multiThreadPoolRes.get(i).get());
@@ -48,6 +57,51 @@ class ThreadPoolImplTest {
     }
 
     @Test
-    void shutdown() {
+    void shutdownSimple() {
+        var isRunning = new AtomicBoolean(false);
+        singleThreadPool.submit(() -> {
+            isRunning.set(true);
+            synchronized (this) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException();
+                } finally {
+                    isRunning.set(false);
+                }
+            }
+            return 0;
+        });
+        singleThreadPool.shutdown();
+        assertFalse(isRunning.get());
+    }
+
+    @Test
+    void countThreads() throws LightExecutionException {
+        var latch = new CountDownLatch(multiThreadPoolSize);
+        int n = multiThreadPoolSize - 1;
+        final Supplier<String> task = () -> {
+            latch.countDown();
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return "done";
+        };
+
+        var multiThreadPoolRes = new ArrayList<LightFuture<String>>(n);
+        for (int i = 0; i < n; i++) {
+            multiThreadPoolRes.add(multiThreadPool.submit(task));
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            assertFalse(multiThreadPoolRes.get(i).isReady());
+        }
+        
+        multiThreadPoolRes.add(multiThreadPool.submit(task));
+        for (int i = n; i >= 0; i--) {
+            assertEquals("done", multiThreadPoolRes.get(i).get());
+        }
     }
 }
